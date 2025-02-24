df6bcc3224021d47bbe5ff53f1ff008ea7de726d
Mirko <mirkocaccavone03@gmail.com>
2025-02-24 19:43:41 +0100

    [feat] Add navigation links to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+// Import di Link e NavLink dalla libreria react-router-dom per la navigazione
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
         <header>
             <div className="nav-bar">
+                {/* NavLink per la navigazione alle pagine */}
                 <NavLink to="/">Homepage</NavLink>
                 <NavLink to="/chisiamo">Chi Siamo</NavLink>
                 <NavLink to="/listpost">Lista dei post</NavLink>
