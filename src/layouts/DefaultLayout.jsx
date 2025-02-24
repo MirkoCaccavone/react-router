7cc1e2113ffef7674072766fdbbb7245a87f72b1
Mirko <mirkocaccavone03@gmail.com>
2025-02-24 19:50:03 +0100

    [feat] add  Header component and Outlet for route rendering in DefaultLayout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,5 +1,7 @@
+// Import di Outlet dalla libreria react-router-dom per il rendering delle rotte figlie
 import { Outlet } from "react-router-dom"
 
+// Import del componente Header
 import Header from "./../components/Header";
 
 
@@ -7,7 +9,9 @@ import Header from "./../components/Header";
 export default function DefaultLayout() {
     return (
         <div>
+            {/* Componente Header visualizzato in tutte le pagine */}
             <Header />
+            {/* Outlet renderizza il componente della rotta corrente */}
             <Outlet />
         </div>
     );
