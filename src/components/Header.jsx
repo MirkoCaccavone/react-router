3494743a283899efe58644000df577f7ce9f4212
Mirko <mirkocaccavone03@gmail.com>
2025-02-25 21:10:27 +0100

    [feat] update Header component navigation links and add 'add Post' link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 // Import di Link e NavLink dalla libreria react-router-dom per la navigazione
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
@@ -8,7 +8,10 @@ export default function Header() {
                 {/* NavLink per la navigazione alle pagine */}
                 <NavLink to="/">Homepage</NavLink>
                 <NavLink to="/chisiamo">Chi Siamo</NavLink>
-                <NavLink to="/listpost">Lista dei post</NavLink>
+                <NavLink to="/posts">Lista dei post</NavLink>
+                <NavLink to="/posts/create">add Post</NavLink>
+
+
 
             </div>
         </header>
